ea09702493531aab1db4552bfe41236eed0305c3
Aigerim Almazova <71431806+moonbell-e@users.noreply.github.com>
2024-05-12 22:46:06 +0300

    Added Dialogue System and changed models

---
+++ Roguelike/Assets/Plugins/Pixel Crushers/Dialogue System/Wrappers/UI/Standard/Dialogue/StandardUISubtitlePanel.cs
@@ -0,0 +1,17 @@
+﻿// Copyright (c) Pixel Crushers. All rights reserved.
+using System;
+using UnityEngine;
+
+namespace PixelCrushers.DialogueSystem.Wrappers
+{
+
+    /// <summary>
+    /// This wrapper class keeps references intact if you switch between the 
+    /// compiled assembly and source code versions of the original class.
+    /// </summary>
+    [HelpURL("http://www.pixelcrushers.com/dialogue_system/manual2x/html/standard_u_i_subtitle_panel.html")]
+    [AddComponentMenu("Pixel Crushers/Dialogue System/UI/Standard UI/Dialogue/Standard UI Subtitle Panel")]
+    public class StandardUISubtitlePanel : PixelCrushers.DialogueSystem.StandardUISubtitlePanel
+    {
+    }
+}
--- Roguelike/Assets/_Scripts/Player/StateMachine/Data/PlayerAnimationDataHash.cs
+++ Roguelike/Assets/_Scripts/Player/StateMachine/Data/PlayerAnimationDataHash.cs
@@ -5,7 +5,9 @@ public class PlayerAnimationDataHash
     private const string Idle = "Idle";
     private const string Run = "Run";
     private const string Dash = "Dash";
-    private const string Attack = "Attack";
+    private const string Attack1 = "Attack1";
+    private const string Attack2 = "Attack2";
+    private const string Attack3 = "Attack3";
     private const string SpecialAttack = "SpecialAttack";
     private const string Speed = "MoveSpeed";
     
@@ -13,7 +15,10 @@ public class PlayerAnimationDataHash
     public int RunHash { get; } = Animator.StringToHash(Run);
     public int DashHash { get; } = Animator.StringToHash(Dash);
     
-    public int AttackHash { get; } = Animator.StringToHash(Attack);
+    public int Attack1Hash { get; } = Animator.StringToHash(Attack1);
+    public int Attack2Hash { get; } = Animator.StringToHash(Attack2);
+    public int Attack3Hash { get; } = Animator.StringToHash(Attack3);
+    
     public int SpecialAttackHash { get; } = Animator.StringToHash(SpecialAttack);
     public int MoveSpeedHash { get; } = Animator.StringToHash(Speed);
 }
